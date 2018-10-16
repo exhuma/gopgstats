8253f54d7adb62a810492f8c58b0f1f77a1e19a8
Michel Albert <michel@albert.lu>
2018-10-16 15:59:04 +0200

    Remove "concrete.go" (have not figured out mocking)

--- stats_test.go
+++ stats_test.go
@@ -5,8 +5,7 @@ import "testing"
 func TestActivity(t *testing.T) {
     fetcher, _ := MakeDefaultFetcher("dbname=exhuma")
     result, _ := fetcher.Activity()
-    expected := []ActivityRow{};
-    if len(result) != len(expected) {
-        t.Errorf("%v != %v", result, expected)
+    if len(result) == 0 {
+        t.Errorf("We did not get any results from the DB")
     }
 }
