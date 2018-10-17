a107136fab935be24e7a27c4e05fc3d43bc78ea6
Michel Albert <michel@albert.lu>
2018-10-17 15:19:24 +0200

    Remove "Activity" stats
    These were just an example

--- types.go
+++ types.go
@@ -1,32 +1,5 @@
 package gopgstats
 
-import (
-	"github.com/lib/pq"
-)
-
-type ActivityRow struct {
-	DatId           int
-	DatName         string
-	PId             int
-	UseSysId        int
-	UseName         string
-	ApplicationName string
-	ClientAddress   string
-	ClientHostname  string
-	ClientPort      int
-	BackendStart    pq.NullTime
-	XactStart       pq.NullTime
-	QueryStart      pq.NullTime
-	StateChange     pq.NullTime
-	WaitEventType   string
-	WaitEvent       string
-	State           string
-	BackendXid      string
-	BackendXmin     string
-	Query           string
-	BackendType     string
-}
-
 type DiskSizeRow struct {
 	DatabaseName string
 	Size         int
