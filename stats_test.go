b1067acfbc4227c18a78dffabc63d0405758d8cc
Michel Albert <michel@albert.lu>
2018-10-17 15:33:08 +0200

    Fixed unit-tests

--- stats_test.go
+++ stats_test.go
@@ -1,20 +1,15 @@
 package gopgstats
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"testing"
 )
 
-func TestActivity(t *testing.T) {
-	fetcher, _ := MakeDefaultFetcher("dbname=exhuma")
-	result, _ := fetcher.Activity()
-	if len(result) == 0 {
-		t.Errorf("We did not get any results from the DB")
-	}
-}
-
 func TestDisksize(t *testing.T) {
-	fetcher, _ := MakeDefaultFetcher("dbname=exhuma")
+	db, _ := sql.Open("postgres", "dbname=exhuma")
+	fetcher := MakeDefaultFetcher(db)
 	result, _ := fetcher.DiskSize()
 	for _, item := range result {
 		fmt.Println("disksize: ", item)
@@ -22,7 +17,8 @@ func TestDisksize(t *testing.T) {
 }
 
 func TestLocks(t *testing.T) {
-	fetcher, _ := MakeDefaultFetcher("dbname=exhuma")
+	db, _ := sql.Open("postgres", "dbname=exhuma")
+	fetcher := MakeDefaultFetcher(db)
 	result, _ := fetcher.Locks()
 	for _, item := range result {
 		fmt.Println("locks: ", item)
